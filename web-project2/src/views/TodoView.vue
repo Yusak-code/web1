fd9b65a37c368e2421e4675fa15820a3f37cba4b
Yusak-code <yusakardianto19@gmail.com>
2025-05-26 11:33:44 +0700

    styling list

--- web-project2/src/views/TodoView.vue
+++ web-project2/src/views/TodoView.vue
@@ -3,11 +3,32 @@
 
     <ul>
         <li v-for="list in todoStore.todoList">
-            {{ list.name }}
-
+            <span>
+                {{ list.name }}
+            </span>
+            <span>
+                <button>done</button>
+            </span>
         </li>
     </ul>
 </template>
+
+<style scoped>
+
+    ul {
+        list-style: none;
+        width: 500px;
+    }
+
+    li {
+        border: solid 2px;
+        margin: 10px;
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+    }
+
+</style>
 
 <script>
 import {useTodoStore} from '../stores/todoStore'
